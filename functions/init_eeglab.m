dd204298a5d3f174c3a13f6e7cbeabaeb50ad1a4
Nebris <giovannilazzaro94@gmail.com>
2025-10-06 11:46:27 +0200

    Update init_eeglab.m
    Implementing the function to initialize eeglab and oper gui. The function have been tested and it works.

--- functions/init_eeglab.m
+++ functions/init_eeglab.m
@@ -1,11 +1,42 @@
-% This script initialize EEGLAB
-% Add EEGLAB to path and start it
+%%% This script initialize and launch EEGLAB
+% 
+% USAGE: 
+%       init_eeglab(eeglab_path)
+%
+% INPUT:
+%        eeglab_path: full path to your EEGLAB installation (see config. file)
+%
+% Description: 
+%             Add EEGLAB to path and start it with GUI
+%             Check if EEGLAB is alrady on the matlab path
+
 
 function init_eeglab(eeglab_path)
-    if nargin < 1
-        error('EEGLAB path not provided.');
-    end
 
-    addpath(genpath(eeglab_path));
-    eeglab; % Start EEGLAB
+%input check
+
+if nargin <1 %check n.input arguments
+   error ("EEGLAB path not provided");
 end
+
+if ~isfolder(eeglab_path)
+   error("the provided EEGLAB path does not exist:\n%s", eeglab_path);
+end
+
+%check if EEGLAB is already on path
+
+if exist ("eeglab","file")
+   fprintf("adding EEGLAB to MATLAB path. \n")
+else
+   fprintf("adding EEGLAB to MATLAB path \n")
+   addpath(genpath(eeglab_path));
+end
+
+% LAUNCH EEGLAB
+fprintf("Launching EEGLAB...\n");
+eeglab; % Start EEGLAB open GUI
+
+end
+
+
+   
