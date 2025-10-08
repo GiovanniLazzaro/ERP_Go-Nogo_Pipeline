8d507bbf2480f6b8bbe56660be1554b672337e1d
Nebris <giovannilazzaro94@gmail.com>
2025-10-08 12:30:15 +0200

    Update main_pipeline.m
    Updated the structure

--- Project/project/main_pipeline.m
+++ Project/project/main_pipeline.m
@@ -1,19 +1,38 @@
 %% ==========================
-%% Main Script - Initialization
+%% Main PIPELINE  - ERP Go/No-Go Project
 %% ==========================
-% This section sets up the project environment by loading user-specific
-% configuration variables and initializing EEGLAB. 
+% This script runs the entire EEG preprocessing pipeline.
+% It sequentially calls scripts from the scripts folder,
+% which in turn use functions from the functions folder.
 % 
 % Steps:
-%   1. Load configuration from config.m (paths, parameters, etc.)
-%   2. Initialize EEGLAB, making all its functions available for use
+%   0. Load configuration from config.m (paths, parameters, etc.)
+%   1. Initialize EEGLAB, making all its functions available for use
+%   2. Load EEG raw data and converts in .set files
 %
 % Note: Users should create their own config.m template based on config.m
 %       and adjust paths according to their system.
+%=============================
 
-%% Load configuration
-config;  % this loads all variables defined in your config.m file
+clear; clc; close all;
+fprintf('\n===== STARTING ERP GO/NO-GO PIPELINE =====\n');
 
-%% Initialize EEGLAB
-init_eeglab(eeglab_path);  % uses the variable defined in config
+%%  Step 0: 
+fprintf('\n[Step 0] Loading configuration...\n');
+cfg = config();
+
+%%  Step 1: 
+fprintf('\n[Step 1] Initializing EEGLAB...\n');
+init_eeglab(eeglab_path);
+
+%% Step 2: 
+fprintf('\n[Step 2] Importing raw EEG files...\n');
+run('script/scritp_load_eeg.m');
+
+%% Step 3:
+
+
+
+
+%%%% add at the end fprintf('\n===== PIPELINE COMPLETED SUCCESSFULLY =====\n'); %%%
 
