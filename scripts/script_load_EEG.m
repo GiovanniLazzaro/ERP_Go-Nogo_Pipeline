91d8d94711aa9bd63700543fcc808617bf89642b
Nebris <giovannilazzaro94@gmail.com>
2025-10-08 12:18:35 +0200

    Update and rename Step1_load_EEG_data.m to script_load_EEG.m

--- scripts/script_load_EEG.m
+++ scripts/script_load_EEG.m
@@ -4,12 +4,11 @@
 % using the function load_EEG.m
 % ========================================================================
 
-clarvars - except cfg; %clear variables except config.
+clearvars; close all; clc;
+
 %% Step 0: Load configuration (if not loaded)
 
-if ~exist('cfg' ,'var')
-   cfg = config();
-end
+cfg = config(); % load parameters and paths
 
 %% Step 1: initialize EEGLAB
 
@@ -17,5 +16,5 @@ init_eeglab(cfg.eeglab_path);
 
 %% Step 2: Load and convert raw EEG files
 fpritf('\n[SCRIPT] Loading raw EEG data and converting to .set format...\n');
-load_eeg(cfg, '*.vhdr')
+load_EEG_files(cfg, '*.vhdr')
 disp('Step 2 completed: EEG data loaded and saved in .set format.');
