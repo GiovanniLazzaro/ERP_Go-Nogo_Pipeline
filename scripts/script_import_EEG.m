8a7acbeab1ab54773a5242467ce4710492cbe81e
Nebris <giovannilazzaro94@gmail.com>
2025-10-15 14:15:47 +0200

    Update script_import_EEG.m

--- scripts/script_import_EEG.m
+++ scripts/script_import_EEG.m
@@ -7,15 +7,19 @@
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-cfg=config()
+%% Step 0: Load configuration (if not loaded)
+
+cfg = config(); % load parameters and paths
+
 %%  Step 1: 
 fprintf('\n[Step 1] Initializing EEGLAB...\n');
 init_eeglab(cfg.eeglab_path);
 
 %% Step 2: 
 fprintf('\n[Step 2] Import EEG raw files and tranform them in .set files')
-cfg=config();
+
 import_EEG(cfg.input_folder,cfg.output_folder,cfg.file_ext);
+
 fprintf('\n All EEG files have been loaded and converted in .set files!\n');
 
 
